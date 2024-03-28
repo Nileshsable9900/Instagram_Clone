700d38ecdd543a70a1712083a3eb8ea47bbafc99
Nilesh <nileshsable9900@gmail.com>
2024-03-28 11:57:31 +0530

    instagram-clone using nextjs

---
+++ util/firbaseConfig.ts
@@ -0,0 +1,16 @@
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+  const firebaseConfig = {
+    apiKey: "AIzaSyAzuFFpD8aISqh8okkAFFneIeB1nRflm2U",
+    authDomain: "insta-997fe.firebaseapp.com",
+    projectId: "insta-997fe",
+    storageBucket: "insta-997fe.appspot.com",
+    messagingSenderId: "1079041110552",
+    appId: "1:1079041110552:web:1a2e4f67213c004d37c96d"
+  };
+};
+
+const app = initializeApp(firebaseConfig);
+
+export default app;
